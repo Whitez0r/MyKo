e3aa0e55c195835a34ca5aa626dfbfcf64b30499
whitez0r <i_mungalov@mail.ru>
2022-03-18 21:57:36 +0300

    Create Reg and Auth design

---
+++ MyKo/Controller/App/AppViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppViewController.swift
+//  MyKo
+//
+//  Created by Илья Мунгалов on 18.03.2022.
+//
+
+import UIKit
+
+class AppViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
